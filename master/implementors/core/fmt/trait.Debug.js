ff5e623b368da02675890a916f86b08cd1213b93
Travis CI User <travis@example.org>
2018-02-18 23:35:11 +0000

    ghp-upload script

---
+++ master/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cargo_ghp_upload"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"cargo_ghp_upload/enum.CargoArgs.html\" title=\"enum cargo_ghp_upload::CargoArgs\">CargoArgs</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"cargo_ghp_upload/struct.Args.html\" title=\"struct cargo_ghp_upload::Args\">Args</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"cargo_ghp_upload/struct.Context.html\" title=\"struct cargo_ghp_upload::Context\">Context</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
