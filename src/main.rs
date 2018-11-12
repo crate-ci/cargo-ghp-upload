012a6bfcadee643f79766031bfd4291656f17a39
Christopher Durham <cad97@cad97.com>
2018-11-11 21:47:05 -0500

    Remove #![forbid]

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![forbid(future_incompatible)]
-#![warn(warnings)]
-
 extern crate quicli;
 use quicli::prelude::*;
 
